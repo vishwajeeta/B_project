458659105d1b45dfa3fb257afddcb6cb13b0d13d
vishwajeeta <gs699542@gmail.com>
2024-03-21 20:07:45 +0530

    updating the frontend with more attractive and responsive design

--- portfolio/frontend_next/app/compontnts/Header.js
+++ portfolio/frontend_next/app/compontnts/Header.js
@@ -1,9 +1,66 @@
+"use client"
 import React from 'react'
+import { motion } from 'framer-motion';
+import Image from 'next/image';
 import './Header.scss';
+
+const scaleVarients={
+  whileInView:{
+    scale:[0,1],
+    opacity:[0,1],
+    transition:{
+      duration:1,
+      ease:'easeInOut'
+    }
+  }
+}
 function Header() {
   return (
-    <div>
-      Header
+    <div className='app__header app__flex'>
+      <motion.div
+      whileInView={{x:[-100,0],opacity:[0,1]}}
+      transition={{duration:0.5}}
+      className='app__header-info'
+      >
+        <div className='app__header-badge'>
+          <div className='badge-cmp app__flex'>
+            <span>Hii</span>
+            <div style={{marginLeft:20}}>
+              <p className='p-text'>Hello ,I am </p>
+              <h1 className='head-text'>Vishwa</h1>
+            </div>
+          </div>
+          <div className='tag-cmp app__flex'>
+            <p className='p-text'>Web Developer</p>
+            <p className='p-text'>FreeLancer</p>
+            
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+      whileInView={{opacity:[0,1]}}
+      transition={{duration:0.5, delayChildren:0.5}}
+      className='app__header-img'
+      >
+
+      <Image src="/vishwaProfile.png" alt='profile_bg' width={90} height={20}/>
+      <motion.img
+      whileInView={{opacity:[0,1]}}
+      transition={{duration:1,ease:'easeInOut'}}
+      src="/vishwaProfile.png"
+      alt="profile_circle"
+      className="overlay_circle"
+      />
+      </motion.div>
+
+      <motion.div
+      varient={scaleVarients}
+      whileInView={scaleVarients.whileInView}
+      className='app__header-circles'
+      >
+
+      </motion.div>
     </div>
   )
 }
--- portfolio/frontend_next/app/compontnts/Navbar.js
+++ portfolio/frontend_next/app/compontnts/Navbar.js
@@ -1,11 +1,14 @@
 "use client"
 import Image from "next/image";
-import {vercel} from "next/image";
+import {HiMenuAlt4,HiX} from 'react-icons/hi';
 import {motion} from "framer-motion";
 import './Navbar.scss';
 import { useState } from "react";
+
 function Navbar() {
-  const [toggle,setToggle]=useState(false);
+
+  const [toggle , setToggle]=useState(false);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -20,14 +23,24 @@ function Navbar() {
       ))}
       </ul>
       <div className="app__navbar-menu">
-        <vercel onclick={()=>setToggle(true)} />
-        {toggle &&(
+        <HiMenuAlt4 onClick={()=>setToggle(true)} />
+
+        {toggle && (
           <motion.div
           whileInView={{x:[300,0]}}
           transition={{duration:0.85,ease:'easeOut'}}
           >
-
+          
+            <HiX onClick={()=>setToggle(false)}/>
+            <ul>
+            {['home','about','work','skills','contact'].map((item)=>(
+        <li key={item}>
+          <a href={'#'+item} onClick={()=>setToggle(false)}>{item}</a>
+          </li>
+      ))}
+      </ul>
           </motion.div>
+          
           )}
       </div>
       </nav>
