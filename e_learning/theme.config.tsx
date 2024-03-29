50a0a6fb67c17cd90200af1a02f55f3bf7695984
vishwajeeta <gs699542@gmail.com>
2024-03-29 13:50:46 +0530

    creating and updating documentation website

---
+++ e_learning/theme.config.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>SecureBlockASVSI</span>,
+  
+  project: {
+    link: 'https://github.com/vishwajeeta',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Pownered by SecureBlockASVSI',
+  },
+  primaryHue:277,
+  
+}
+
+export default config
