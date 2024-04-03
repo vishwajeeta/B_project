ab092bde5a716af113707b35f865962d531b04b6
vishwajeeta <gs699542@gmail.com>
2024-04-03 17:08:43 +0530

    adding some front-end functionalities

--- portfolio/frontend_next/app/compontnts/Work.js
+++ portfolio/frontend_next/app/compontnts/Work.js
@@ -1,10 +1,27 @@
-import React from 'react'
+"use client"
+import React,{useState,useEffect} from 'react';
+import {AiFillEye,AiFillGithub} from 'react-icons/ai';
+import {motion} from 'framer-motion';
 import './Work.scss';
 function Work() {
+  const handleWorkFilter =(item)=>{
+    const [activeFilter,setActiveFilter]=useState('All')
+  }
   return (
-    <div>
-      work
-    </div>
+    <>
+      <h2 className='head-text'>My Creative<span> Portfolio</span><br/> means <span>Good Business</span> section</h2>
+      <div className='app__work-filter'>
+        {['Web3','Blockchain','django','Next.js','All'].map((item,index)=>(
+          <div
+          key={index}
+          onClick={()=> handleWorkFilter(item)}
+          className={'app__work-filter-item app__flex p_text ${activeFilter=== item ? "item-active" : ""}'}
+          >
+            {item}
+            </div>
+        ))}
+      </div>
+    </>
   )
 }
 
