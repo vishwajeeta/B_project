be992d5322fcc84394eb5b12e08bfaa906c79754
vishwajeeta <gs699542@gmail.com>
2024-09-02 17:55:29 +0530

    updated testmonials

--- portfolio/frontend_next/app/compontnts/Testimonial.js
+++ portfolio/frontend_next/app/compontnts/Testimonial.js
@@ -3,11 +3,60 @@ import React,{useState,useEffect} from 'react'
 import { motion } from 'framer-motion';
 import { HiChevronLeft,HiChevronRight } from 'react-icons/hi';
 import './Testimonial.scss';
+
+
+const testimonial_data = [
+  {
+    name: "solidity",
+    BgColor:"white",
+    icon: "./about_icons/solidity.webp",
+    
+  },
+  {
+    name: "Next.js",
+    BgColor:"white",
+    icon: "./about_icons/next.svg",
+    
+  },
+  {
+    name: "django",
+    BgColor:"white",
+    icon: "./about_icons/django.webp",
+    
+  },
+  {
+    name: "flutter",
+    BgColor:"white",
+    icon: "./about_icons/flutter.webp",
+    
+  },
+];
+
 function Testimonial() {
+  const [iknow,setIknow]=useState([]);
+
+  useEffect(()=>{
+    setIknow(testimonial_data);
+  },[]);
+
+  const [currentIndex,setCurrentIndex]=useState(0);
+  const test=iknow[currentIndex];
   return (
-    <div>
-      Testimonial
-    </div>
+    <>
+      {iknow.length && (
+        <>
+        <div className='app__testimonial-item app__flex'>
+          <img src={test.icon}/>
+          <div className='app__testimonial-content'>
+            <p className='p-text'>{test.name}</p>
+            <div>
+              <h4 className='bold-test'>{test.name}</h4>
+            </div>
+          </div>
+        </div>
+        </>
+      )}
+    </>
   )
 }
 
