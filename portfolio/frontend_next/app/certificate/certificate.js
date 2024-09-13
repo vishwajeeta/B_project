aa231d82904fe2f31fe830c7bc8083f66e46e727
vishwajeeta <gs699542@gmail.com>
2024-09-13 21:09:22 +0530

    optimized and removed wornings

--- portfolio/frontend_next/app/certificate/certificate.js
+++ portfolio/frontend_next/app/certificate/certificate.js
@@ -22,12 +22,12 @@ function Certificates() {
     <center>
 
 <div className="flex m-auto justify-center items-center flex-col md:flex-row lg:flex-row xl:flex-row">
-<img src={`https://lh3.googleusercontent.com/UDiaF1JgtVpL05mGnoELd7Wz2sQP0CQQk2pJ72rYJwynRn5M-J7W89tHVo6U0tVQ9zC_l8ynkonS82iw6WGfpOOZ__ukDV5ulQ`} alt={'certificate'} fill  className='images'/>
-<img src={`https://lh3.googleusercontent.com/fRkqN2gbU44b0WlEIDqFooQuUqDqzrfEAPVPCuJEtGQFevnk1wATA4c3H_uzMKU4oN710vddKmxcro0Sh3nLgdUvlm1dUh4xk9kR=k`} alt={'certificate1'}  fill className='images'/> 
+<img src={`https://lh3.googleusercontent.com/UDiaF1JgtVpL05mGnoELd7Wz2sQP0CQQk2pJ72rYJwynRn5M-J7W89tHVo6U0tVQ9zC_l8ynkonS82iw6WGfpOOZ__ukDV5ulQ`} alt={'certificate'} fill="true"  className='images'/>
+<img src={`https://lh3.googleusercontent.com/fRkqN2gbU44b0WlEIDqFooQuUqDqzrfEAPVPCuJEtGQFevnk1wATA4c3H_uzMKU4oN710vddKmxcro0Sh3nLgdUvlm1dUh4xk9kR=k`} alt={'certificate1'}  fill="true" className='images'/> 
 </div>
-    {certificate1.map((certificate1)=>(
+    {certificate1.map((certificate1,index)=>(
   
-  <Image src={`/Certificates/${certificate1}`} alt={certificate1} width={mywidth} height={mywidth} optimized className='images'/>
+  <Image src={`/Certificates/${certificate1}`} key={index} alt={certificate1} width={mywidth} height={mywidth} optimized="true" className='images'/>
   
 ))}
 </center>
