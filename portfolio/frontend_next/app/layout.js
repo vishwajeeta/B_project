c004489d284e428246293b5b2965345365c36533
vishwajeeta <gs699542@gmail.com>
2024-11-28 21:22:26 +0530

    updated

--- portfolio/frontend_next/app/layout.js
+++ portfolio/frontend_next/app/layout.js
@@ -1,39 +1,52 @@
 // import { Inter } from "next/font/google";
 import "./globals.css";
-
+import ogImage from "./opengraph-imgae.png";
 // const inter = Inter({ subsets: ["latin"] });
 
 // PWD setting
 export const metadata = {
   title: "My Portfolio App",
   description: "vishwajeet Portfolio",
-  generator:"Next.js",
-  manifest:"/manifest.json",
-  keywords:["next.js","nextjs13","next13","pwa","next-pwa"],
-  authors:[
-    {name:"vishwajeet"},
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["next.js", "nextjs13", "next13", "pwa", "next-pwa"],
+  authors: [
+    { name: "vishwajeet" },
     {
-      name:"vishwajeet",
-      url:"https://b-project-nine.vercel.app/",
+      name: "vishwajeet",
+      url: "https://portfolio-chi-inky-28.vercel.app/",
     },
   ],
-  openGraph:{
-    images:'./opengraph-image.png',
+  metadataBase: new URL("https://portfolio-chi-inky-28.vercel.app"),
+  openGraph: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
   },
-  icons:[
-    {rel:"apple-touch-icon", url:"ios/128.png"},
-    {rel: "icon",url:"ios/128.png"},
+  icons: [
+    { rel: "apple-touch-icon", url: "ios/128.png" },
+    { rel: "icon", url: "ios/128.png" },
   ],
-  twitter:{
-    card:"summary_large_image"
-  }
+  twitter: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
 };
 
 export const viewport = {
-  themeColor:[
-    {media:"(prefers-color-scheme:light",color:"#fff"}],
-    viewport:"minimum-scale=1, initial-scale=1, width=device-width,shrink-to-fit=no,viewport-fit=cover",
-}
+  themeColor: [{ media: "(prefers-color-scheme:light", color: "#fff" }],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width,shrink-to-fit=no,viewport-fit=cover",
+};
 
 export default function RootLayout({ children }) {
   return (
