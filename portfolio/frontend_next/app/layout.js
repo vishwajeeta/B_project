504fb9c983645c87c4dfc2c1a40b54e389fb366c
vishwajeeta <gs699542@gmail.com>
2024-11-29 16:04:00 +0530

    updated

--- portfolio/frontend_next/app/layout.js
+++ portfolio/frontend_next/app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
         <meta property="og:title" content="My Portfolio App which has all my projects." />
         <meta property="og:description" content="vishwajeet Portfolio. it contains all my projects with additional informations." />
         <meta property="og:image" content="https://portfolio-chi-inky-28.vercel.app/opengraph-image.png" />
-        opengraph-image
+        
         <meta property="og:url" content="https://portfolio-chi-inky-28.vercel.app" />
         <meta property="og:type" content="website" />
 
