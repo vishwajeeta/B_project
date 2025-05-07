5eaa45a623882693f44bf1e80107e1e3405d9fdc
vishwajeeta <gs699542@gmail.com>
2025-05-07 17:33:08 +0530

    added new path

---
+++ portfolio/frontend_next/app/marketplace/page.js
@@ -0,0 +1,17 @@
+function page() {
+  return (
+    <div>
+      <a href="../">home</a>
+    <h1><u>
+      Marketplace
+      </u>
+    </h1>
+    <hr/>
+  
+    </div>
+  )
+}
+
+
+
+export default page
