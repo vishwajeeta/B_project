c2ccb85b2952b7014c9a002af80e97a9df095202
web3solidity <amruthaeducationyelhanka23@gmail.com>
2025-12-20 10:15:44 +0000

    updated sitemap

--- portfolio/frontend_next/app/sitemap.js
+++ portfolio/frontend_next/app/sitemap.js
@@ -1,18 +1,6 @@
-import { challenges } from './challenges/challanges'
 
 export default async function sitemap() {
-  const baseUrl = 'https://vishwajeetportfolio.vercel.app'
-
-  const challengeUrls = challenges.map((challenge) => {
-    const slug = `${challenge.id}-${challenge.slug}` 
-    // OR if you don't have slug field:
-    // const slug = `${challenge.id}-${challenge.title.toLowerCase().replace(/\s+/g, '-')}`
-
-    return {
-      url: `${baseUrl}/challenges/${slug}`,
-      lastModified: new Date().toISOString(),
-    }
-  })
+  const baseUrl = "https://vishwajeetportfolio.vercel.app";
 
   return [
     {
@@ -28,41 +16,29 @@ export default async function sitemap() {
       lastModified: new Date().toISOString(),
     },
     {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date().toISOString(),
+    },
+    {
       url: `${baseUrl}/challenges`,
       lastModified: new Date().toISOString(),
     },
     {
-      url: `${baseUrl}/blog`,
+      url: `${baseUrl}/challenges/1-solidity-simple-bug`,
       lastModified: new Date().toISOString(),
     },
-    ...challengeUrls,
-  ]
+    {
+      url: `${baseUrl}/challenges/2-solidity-function-bug`,
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: `${baseUrl}/challenges/3-gas-optimized-storage`,
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: `${baseUrl}/challenges/4-gas-optimization-and-storage-refunds`,
+      lastModified: new Date().toISOString(),
+    }
+    // Add more routes as needed
+  ];
 }
-
-// export default async function sitemap() {
-//   const baseUrl = "https://vishwajeetportfolio.vercel.app";
-
-//   return [
-//     {
-//       url: `${baseUrl}/`,
-//       lastModified: new Date().toISOString(),
-//     },
-//     {
-//       url: `${baseUrl}/certificate`,
-//       lastModified: new Date().toISOString(),
-//     },
-//     {
-//       url: `${baseUrl}/marketplace`,
-//       lastModified: new Date().toISOString(),
-//     },
-//     {
-//       url: `${baseUrl}/challenges`,
-//       lastModified: new Date().toISOString(),
-//     },
-//     {
-//       url: `${baseUrl}/blog`,
-//       lastModified: new Date().toISOString(),
-//     },
-//     // Add more routes as needed
-//   ];
-// }
